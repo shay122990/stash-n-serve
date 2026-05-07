6ddef0c18ef9cf1c80a263cfcc464e43bad06ab6
shay asanova <moeshakasanova@gmail.com>
2026-05-08 00:10:06 +0400

    ajax call refactored

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,4 +1,5 @@
-import { TIMEOUT_SEC } from "./config";
+import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "./config.js";
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -8,6 +9,29 @@ const timeout = function (s) {
   });
 };
 
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/*
 export const getJSON = async function (url) {
   try {
     const fetchPro = fetch(url);
@@ -24,9 +48,9 @@ export const getJSON = async function (url) {
 export const sendJSON = async function (url, uploadData) {
   try {
     const fetchPro = fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(uploadData),
     });
@@ -40,3 +64,4 @@ export const sendJSON = async function (url, uploadData) {
     throw err;
   }
 };
+*/
